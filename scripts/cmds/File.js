68f54150e1cf3d1ff0d06249de938c53137ee44d
lost-aizen-xemon <ashredwan03@gmail.com>
2024-12-24 02:23:13 +0600

    Update File.js

--- scripts/cmds/File.js
+++ scripts/cmds/File.js
@@ -1,5 +1,4 @@
 const fs = require('fs-extra');
-
 const { getPrefix } = global.utils;
 
 module.exports = {
@@ -8,37 +7,43 @@ module.exports = {
     version: '1.0',
     role: 0,
     coolDown: 5,
-    author: 'UPoL The MiMis Momo ',
+    author: 'Redwan',
     category: 'Admin',
     shortDescription: {
       en: 'sending file'
     },
     longDescription: {
-      en: 'Sending file form bot scripts',
+      en: 'Sending file from bot scripts',
     },
   },
   onStart: async function ({ api, event, args, message }) {
-   const permission = ['100094189827824', '100094189827824'];
-    if (!permission.includes(event.senderID)) return api.sendMessage('Only Bot Admin\'s can use this command. My Sensei UPol can do this.', event.threadId, event.messageId);
-    
+    const permission = ['100094189827824'];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage('এই কমান্ডটি শুধু অ্যাডমিনরা ব্যবহার করতে পারবেন।', event.threadId, event.messageId);
+    }
+
     const { threadID, messageID } = event;
     const prefix = getPrefix(threadID);
     const commandName = this.config.name;
     const command = prefix + commandName;
+
     if (args.length === 0) {
-      return message.reply(`file এর নাম কে দিবে ?. Use: ${command} <file_name>`);
+      return message.reply(`আমি ${commandName} ফাইলটি খুঁজে দেখব।`);
     }
-    const fileName = args[0];
+
+    const fileName = args.length > 0 ? args[0] : this.config.name;
     const filePath = `${__dirname}/${fileName}`;
+
     if (!fs.existsSync(filePath)) {
-      return message.reply(`File ${fileName} নাই নাম ঠিক দিছস তো ??.`);
+      return message.reply(`ফাইল ${fileName} পাওয়া যায়নি। নামটি সঠিক আছে তো?`);
     }
+
     try {
       const fileData = fs.readFileSync(filePath, 'utf-8');
       api.sendMessage(fileData, threadID, messageID);
     } catch (error) {
       console.error(error);
-      message.reply(`তোর file e problem আছে চেক দে.`);
+      message.reply(`ফাইলে সমস্যা হয়েছে। আবার চেক করুন।`);
     }
   }
 };
