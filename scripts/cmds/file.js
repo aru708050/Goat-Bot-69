afe54a2e1d7da5d6a721ed5b06998726f08f1fc0
aru7080 <arutanjina1@gmail.com>
2025-06-26 07:20:37 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61556251307831"];
+		const permission = ["61577340013899"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage("bambola uira ja sandar pola🐥🔪", event.threadID, event.messageID);
 		}
